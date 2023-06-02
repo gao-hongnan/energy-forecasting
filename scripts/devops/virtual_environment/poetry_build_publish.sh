1e7a0ac32bd84b0bed9d3a2abc5c680ea6156c55
gao-hongnan <hongnangao@gmail.com>
2023-06-02 20:50:04 +0800

    scripts: add poetry_build_publish.sh to build and publish to pypi.

---
+++ scripts/devops/virtual_environment/poetry_build_publish.sh
@@ -0,0 +1,3 @@
+# https://www.digitalocean.com/community/tutorials/how-to-publish-python-packages-to-pypi-using-poetry-on-ubuntu-22-04
+poetry config pypi-token.pypi <API_TOKEN>
+poetry publish --build
